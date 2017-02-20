e6d52ae601d62db818ab0747bedca1c928050807
Adamouization <aj645@bath.ac.uk>
2017-02-20 15:52:44 +0000

    selected basic code for views/css/js without bootstrap styling

---
+++ js/range-slider-value.js
@@ -0,0 +1,10 @@
+/**
+ * Created by adam on 12/02/2017.
+ */
+
+/**
+ * Updates the value of an input text field when the value in the input range slider changes.
+ */
+function updateTextInput(val) {
+    document.getElementById('textInput').value = val;
+}
