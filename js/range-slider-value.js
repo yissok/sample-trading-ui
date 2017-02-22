8734b1f87dec8734faa36619d9366e501e7bdc53
Adamouization <aj645@bath.ac.uk>
2017-02-22 00:25:36 +0000

    Strategy generation more responsive + layout improved

--- js/range-slider-value.js
+++ js/range-slider-value.js
@@ -8,3 +8,18 @@
 function updateTextInput(val) {
     document.getElementById('textInput').value = val;
 }
+
+/**
+ * Updates the value of a h5 header when a new value in the select placeholder is chosen.
+ */
+function updateSelectedStock() {
+    var stock = document.getElementById('stocks').value;
+    document.getElementById('selected-stock').innerText = stock;
+}
+
+/**
+ * Creates an alert when the button "buy" is clicked.
+ */
+function buttonBuyStock() {
+    alert("Stock purchased");
+}
