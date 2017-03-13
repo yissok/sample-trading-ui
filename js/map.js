7bfe84b42d3f81be6dd64ca0f1210d3b5b9adb3c
Adamouization <aj645@bath.ac.uk>
2017-03-13 15:26:15 +0000

    code refactoring + page linking debugging

--- js/map.js
+++ js/map.js
@@ -1,87 +1,40 @@
 var map;
 
-var pinDesc=[];
-var pinLat=[];
-var pinLng=[];
+var pinDesc = [];
+var pinLat = [];
+var pinLng = [];
 
-/**
- * Created by adam on 10/03/2017.
- */
-// This example creates circles on the map, representing populations in North America.
+function initMap() {
+    $.getJSON("myPins.json", function (json) {
+        var pinDesc = new Array()
+        for (i = 0; i < 3; i++)
+            pinDesc[i] = new Array()
 
-//First, create an object containing LatLng and population for each city.
-var citymap = {
-        chicago: {
-            center: {lat: 41.878, lng: -87.629},
-            population: 2714856
-        },
-        newyork: {
-            center: {lat: 40.714, lng: -74.005},
-            population: 8405837
-        },
-        losangeles: {
-            center: {lat: 34.052, lng: -118.243},
-            population: 3857799
-        },
-        vancouver: {
-            center: {lat: 49.25, lng: -123.1},
-            population: 603502
+        for (var i = 0; i < json.pins.length; i++) {
+            pinDesc[0][i] = json.pins[i].lat;
+            pinDesc[1][i] = json.pins[i].lng;
+            pinDesc[2][i] = json.pins[i].desc;
         }
-    };
-
-function initMap()
-{
-    $.getJSON("myPins.json", function(json)
-    {
-        var pinDesc=new Array()
-        for (i=0; i <3; i++)
-            pinDesc[i]=new Array()
-
-        for(var i = 0; i < json.pins.length; i++)
-        {
-            pinDesc[0][i]=json.pins[i].lat;
-            pinDesc[1][i]=json.pins[i].lng;
-            pinDesc[2][i]=json.pins[i].desc;
-        }
-        /*for(var i = 0; i < 5; i++)
-        {
-            document.getElementById('demo').innerHTML += pinDesc[0][i];
-            document.getElementById('demo').innerHTML += "<br>";
-            document.getElementById('demo').innerHTML += pinDesc[1][i];
-            document.getElementById('demo').innerHTML += "<br>";
-            document.getElementById('demo').innerHTML += pinDesc[2][i];
-            document.getElementById('demo').innerHTML += "<br>";
-            document.getElementById('demo').innerHTML += "<br>";
-        }*/
-        
-        
-        
-        
-        
-        
         // Create the map.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 1,
-        center: {lat: 0, lng: 0},
-        mapTypeId: 'terrain'
-    });
-
-    // Construct the circle for each value in citymap.
-    // Note: We scale the area of the circle based on the population.
-    for(var i = 0; i < json.pins.length; i++)
-    {
-        // Add the circle for this city to the map.
-        var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: {lat: parseInt(pinDesc[0][i]), lng: parseInt(pinDesc[1][i])},
-            radius: 1000000
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 1,
+            center: {lat: 0, lng: 0},
+            mapTypeId: 'terrain'
         });
-    }
-
+        // Construct the circle for each value in citymap.
+        // Note: We scale the area of the circle based on the population.
+        for (var i = 0; i < json.pins.length; i++) {
+            // Add the circle for this city to the map.
+            var cityCircle = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: map,
+                center: {lat: parseInt(pinDesc[0][i]), lng: parseInt(pinDesc[1][i])},
+                radius: 1000000
+            });
+        }
     });
 }
